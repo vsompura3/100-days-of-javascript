270904e728048e0595bc3d053c1ccbd2884c0def
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2021-10-10 17:41:45 +0530

    Initial Commit

---
+++ Day01/script.js
@@ -0,0 +1,31 @@
+// Select the elements from the page
+const container = document.getElementById('container');
+const text = document.querySelector('span');
+const button = document.getElementById('generate-color');
+
+// Array for Hex Color [letters and digits]
+const hexCodes = [1,'A',2,'B',3,'C',4,'D',5,'E',6,'F']
+
+// Function that generates random Color
+function generateColor() {
+  // Initial value with '#'
+  let colorCode = '#';
+  // Running loop 6 times to generate hex color
+  for(let i = 0; i < 6; i+=1){
+    // Generate random code for hexColor Array
+    const randomNumber = Math.floor(Math.random() * hexCodes.length);
+    colorCode += hexCodes[randomNumber];
+  }
+
+  // Set container's background as generated hexcolor
+  container.style.backgroundColor = colorCode;
+  // Set the text of the span as colorCode
+  text.textContent = colorCode;
+}
+
+// Generate the backgroundColor when button is clicked
+button.addEventListener('click', generateColor);
+
+
+
+
